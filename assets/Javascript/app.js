8a5b568b8a039248c0b57051e7a20784d9a3eec4
MrSpahceman <palacc25@gmail.com>
2017-08-26 17:31:20 -0700

    new

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -1,4 +1,52 @@
+// * You'll create a trivia form with multiple choice or true/false options (your choice).
+//booleans or if else
+
+// * The player will have a limited amount of time to finish the quiz. 
+//timer setTimeout(); setInterval()function var (){}
+
+//   * The game ends when the time runs out. The page will reveal the number of questions that players answer correctly and incorrectly.
+
+// * Don't let the player pick more than one answer per question.
+
+// * Don't forget to include a countdown timer.
+
+
+
 $(document).ready(){
 
-	
-}
+$(".panel-body").HTML("hello");
+};
+
+// //  The decrement function.
+//     function decrement() {
+
+//       //  Decrease number by one.
+//       number--;
+
+//       //  Show the number in the #show-number tag.
+//       $("#show-number").html("<h2>" + number + "</h2>");
+
+
+//       //  Once number hits zero...
+//       if (number === 0) {
+
+//         //  ...run the stop function.
+//         stop();
+
+//         //  Alert the user that time is up.
+//         alert("Time Up!");
+//       }
+//     }
+
+//     //  The stop function
+//     function stop() {
+
+//       //  Clears our intervalId
+//       //  We just pass the name of the interval
+//       //  to the clearInterval function.
+//       clearInterval(intervalId);
+//     }
+
+
+//     //  Execute the run function.
+//     run();
